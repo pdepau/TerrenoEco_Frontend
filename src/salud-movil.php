ee809cbae641b47e1537549864e1217b6fa9363e
Pau Blanes Climent <blanespau@gmail.com>
2021-12-17 09:57:18 +0100

    maquetacion salud

--- src/salud-movil.php
+++ src/salud-movil.php
@@ -6,30 +6,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SALUD | Terreno Eco</title>
+    <title>MAPA | Terreno Eco</title>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="css/header-menu.css">
     <link rel="stylesheet" href="css/salud.css">
 
 
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-    <style>
-        .nav-informacion a {
-            color: #830053;
-        }
-    </style>
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+
 </head>
 
 <body>
 
-    <?php include_once 'header-menu.php' ?>
-
-    <div id="contenedor" class="ordenador">
-        <div id="textoCO">
+    <div id="contenedor" style="margin:4px;height:100%">
+<div id="textoCO">
         <h1>Efectos del CO:</h1>
                 <br>
                 <p> Pequeñas exposiciones podrían producir un intenso dolor de cabeza
@@ -58,7 +54,7 @@
                 </p>
                 <br>
                 <div class="tabla">
-                    <table class="tablaOrdenador" >
+                    <table  >
                         <thead>
                             <tr>
                                 <th>Concentración de
@@ -663,7 +659,7 @@
                         <br>
 
                         <div class="tabla">
-<table>
+                            <table>
                                 <thead>
                                     <tr>
                                         <th>Concentración de ozono</th>
@@ -735,6 +731,11 @@
 
     </div>
 
-</body>
 
+
+
+    </div>
+
+
+</body>
 </html>
