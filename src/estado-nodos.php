cd182e1353953a048b9db2e7019c7edb5823458d
Adrian <admallla@epsg.upv.es>
2021-12-21 10:42:36 +0100

    Maquetación básica

---
+++ src/estado-nodos.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MAPA | Terreno Eco</title>
+
+    <link rel="stylesheet" href="css/header-menu.css">
+    <link rel="stylesheet" href="css/estado-nodos.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <script src="https://kit.fontawesome.com/a81368914c.js"></script>
+
+    <style>
+        .nav-estado-nodos a {
+            color: #830053;
+        }
+    </style>
+</head>
+
+
+<body>
+
+    <?php include_once 'header-menu.php' ?>
+
+    <div class="contenedor">
+
+        <div class="lista-nodos">
+
+            <div class="nodo">
+                <div class="categoria-nodo">
+                    <div class="leyenda-nodo">Estado :</div>
+                    <div class="estado-nodo">
+                        Perfe
+                    </div>
+                </div>
+                <div class="categoria-nodo">
+                    <div class="leyenda-nodo">ID Nodo :</div>
+                    <div class="id-nodo">11231</div>
+                </div>
+                <div class="categoria-nodo">
+                    <div class="leyenda-nodo">Usuario :</div>
+                    <div class="usuario-nodo">
+
+                        Juan Ramon De la Rosa
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
