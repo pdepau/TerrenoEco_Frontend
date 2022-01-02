0dc1dd5e50dc4dfe4c7248d7f08d1a06f9dd9719
Luis Belloch <luis.bellochmartinez@gmail.com>
2022-01-02 12:34:27 +0100

    login basico en html

--- src/js/proxy.js
+++ src/js/proxy.js
@@ -20,7 +20,7 @@ async function obtenerTodasLasMediciones(cb) {
         return cb(JSON.stringify(json));
       });
     } else {
-      
+      console.error("Error al obtener mediciones")
     }
   });
 }
@@ -38,7 +38,7 @@ async function obtenerTodasLasMediciones(cb) {
         return cb(JSON.stringify(json));
       });
     } else {
-      
+      console.error("Error al obtener nodos");
     }
   });
 }
@@ -56,7 +56,7 @@ async function obtenerTodasLasMediciones(cb) {
         return cb(JSON.stringify(json));
       });
     } else {
-      
+      console.error("No se han podido tomar las ultimas mediciones del usuario");
     }
   });
 }
@@ -82,103 +82,6 @@ async function obtenerMedicionesAcotadas(data, cb) {
   });
 }
 /**
- * mediciones:[medicion],
- * factor:Z =>
- *      interpolarMediciones()
- * mediciones:[medicion] <=
- * 
- * @param {[medicion]} mediciones de base
- * @param {number} factor para crear nuevas mediciones
- * @returns lista de medidas interpoladas
- */
-/* NO ESTA EN USO
-function interpolarMediciones(mediciones, factor) {
-  let interpolados = [];
-  let points = [];
-  const j_mediciones = JSON.parse(mediciones);
-  j_mediciones.forEach((medicion) => {
-    points.push([medicion.latitud, medicion.longitud, medicion.valor]);
-    // Insertamos los valores de la entrada en la misma salida
-    interpolados.push({
-      lat: medicion.latitud,
-      lng: medicion.longitud,
-      valor: medicion.valor,
-    });
-  });
-
-  // Toma valores de las mediciones minimo y maximo para acotar la interpolacion
-  // por el factor
-  const LatMax = Math.max.apply(
-    Math,
-    j_mediciones.map(function (o) {
-      return o.latitud;
-    })
-  );
-  const LonMax = Math.max.apply(
-    Math,
-    j_mediciones.map(function (o) {
-      return o.longitud;
-    })
-  );
-  const LatMin = Math.min.apply(
-    Math,
-    j_mediciones.map(function (o) {
-      return o.latitud;
-    })
-  );
-  const LonMin = Math.min.apply(
-    Math,
-    j_mediciones.map(function (o) {
-      return o.longitud;
-    })
-  );
-
-  // Crea los valores de lat y lon para los datos discretos, el factor es la
-  // cantidad de medidas de salida entre medidas de entrada
-  const cantidad = j_mediciones.length * factor;
-  const diferencia = LonMax - LonMin;
-
-  const escalon = diferencia / cantidad;
-
-  // Para cada latitud
-  for (let i = 0; i < cantidad; i++) {
-    // Para cada longitud
-    for (let j = 0; j < cantidad; j++) {
-      // Averiguamos la distancia del punto actual respecto a los puntos
-      // input
-      // Punto actual, calculado a partir del punto minimo y sumando escalones
-      let nPunto = new Punto(LatMin + escalon * i, LonMin + escalon * j);
-
-      // Para cada punto input averiguamos la distancia y despues
-      // calculamos la media ponderada usando pesos
-      let total = 0;
-      let totalPesos = 0;
-      points.forEach((p) => {
-        let aPunto = new Punto(p[0], p[1]);
-        let distancia = nPunto.distancia(aPunto);
-        // Calcula el peso que tiene aPunto en el que estamos
-        // calculando, que es inversamente proporcional a la
-        // cantidad de nodos por los que pasa antes de llegar a
-        // nPunto
-        // p[2] es la magnitud
-        total += p[2] * (distancia / escalon);
-        totalPesos += distancia / escalon;
-      });
-
-      let mediaPonderada = total / totalPesos;
-
-      interpolados.push({
-        lat: LatMin + escalon * i,
-        lng: LonMin + escalon * j,
-        valor: mediaPonderada,
-      });
-    } // for j
-  } // for i
-
-  return interpolados;
-} // ()
-  */
-/**
  * id:Z =>
  *      obtenerTipo()
  * Tipo:Tipo <=
@@ -193,7 +96,46 @@ function obtenerTipo(id, cb) {
         return cb(JSON.stringify(json));
       });
     } else {
-      document.getElementById("cuerpo").innerHTML = "Error 404";
+      console.error("Error al tomar el tipo");
+    }
+  });
+}
+
+/**
+ * formulario =>
+ *      iniciarSesion()
+ * VoF <=
+ * 
+ * Inicia sesion en el servidor
+ * 
+ * @param {form} formulario 
+ */
+function iniciarSesion() {
+  const form = document.querySelector('form');
+  const formData = new FormData(form);  
+  /*Cuerpo del formulario: 
+  [
+    {"name":"correo","value":"correo@gmail.com"},
+    {"name":"password","value":"contraseña"}
+  ]
+  */
+  let payload = {
+    correo: formData.get("correo"),
+    password: formData.get("password")
+  }
+  let cuerpo = JSON.stringify(payload);
+  console.log(cuerpo);
+  fetch(url + `login`, { 
+    method: "POST", 
+    body: cuerpo, 
+    headers: {"Content-type": "application/json; charset=UTF-8"} 
+  }).then((response) => {
+    if (response.ok) {
+      response.json().then((json) => {
+        return cb(JSON.stringify(json));
+      });
+    } else {
+      console.error("Error al iniciar sesión");
     }
   });
 }
